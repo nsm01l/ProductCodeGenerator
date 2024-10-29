39a2b1bc734b3356d38051681f3200635c99aa5c
nsm01l <nsm01l@hotmail.com>
2024-10-29 21:39:04 +0900

    プロジェクト追加

---
+++ ProductCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ProductCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(12, 25);
+            this.txtPattern.Multiline = true;
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPattern.Size = new System.Drawing.Size(260, 70);
+            this.txtPattern.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Pattern";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Num";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(46, 106);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 19);
+            this.txtNum.TabIndex = 3;
+            this.txtNum.Text = "1";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 131);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCode.Size = new System.Drawing.Size(260, 118);
+            this.txtCode.TabIndex = 5;
+            this.txtCode.DoubleClick += new System.EventHandler(this.txtCode_DoubleClick);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(77, -1);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(47, 20);
+            this.btnHelp.TabIndex = 6;
+            this.btnHelp.Text = "&Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(165, 102);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 4;
+            this.btnGen.Text = "&Gen";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPattern);
+            this.Name = "Form1";
+            this.Text = "ProductCodeGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnGen;
+    }
+}
+
---
+++ ProductCodeGenerator/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ProductCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        private readonly string TITLE = "ProductCodeGenerator";
+        private readonly Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// btnHelpが押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A: Alphabet\nN: Num", this.TITLE);
+        }
+
+        /// <summary>
+        /// btnGen が押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            var tptn = txtPattern.Text.Replace("\r\n", "\n").Split('\n');
+
+            int num;
+            if (!int.TryParse(txtNum.Text, out num))
+            {
+                MessageBox.Show("Num is strange.", this.TITLE);
+                return;
+            }
+
+            var ptn = new List<string>();
+            foreach(var t in tptn)
+            {
+                string v = t.Trim();
+                if (t.Length > 0)
+                {
+                    ptn.Add(t);
+                }
+            }
+            if (ptn.Count == 0)
+            {
+                MessageBox.Show("No pattern.", this.TITLE);
+                return;
+            }
+
+            txtCode.Text = generateProductCode(num, ptn);
+        }
+
+        /// <summary>
+        /// コードを生成
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="pattern"></param>
+        private string generateProductCode(int num, List<string> pattern)
+        {
+            var tret = new List<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string ptr = pattern[rnd.Next(pattern.Count)];  // パターン決定
+                int len = ptr.Length;
+
+                var t = "";
+                for (int j = 0; j < len; j++)
+                {
+                    switch(ptr[j])
+                    {
+                        case 'A':
+                            t += Convert.ToChar('A' + rnd.Next(26));
+                            break;
+
+                        case 'N':
+                            t += rnd.Next(10).ToString();
+                            break;
+
+                        default:
+                            t += ptr[j];
+                            break;
+                    }
+                }
+
+                // 重複チェック
+                bool isDup = false;
+                for(int j = tret.Count - 1; j >= 0; j--)
+                {
+                    if(tret[j] == t)
+                    {
+                        isDup = true;
+                        break;
+                    }
+                }
+                if (!isDup)
+                {
+                    tret.Add(t);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            string ret = "";
+            for (int i = 0; i < num; i++)
+            {
+                ret += tret[i] + "\r\n";
+            }
+            return ret.Trim();
+        }
+
+        /// <summary>
+        /// btnCode をダブルクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtCode_DoubleClick(object sender, EventArgs e)
+        {
+            txtCode.SelectAll();
+        }
+    }
+}
